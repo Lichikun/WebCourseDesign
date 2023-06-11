910fa459e379c31396e1ba1333134afb20e62f75
yaokanghe <249019131@qq.com>
2023-06-11 15:03:39 +0800

    Add User

---
+++ src/main/java/com/example/petshop/service/UserService.java
@@ -0,0 +1,26 @@
+package com.example.petshop.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.example.petshop.entity.User;
+
+import java.util.List;
+
+/**
+* <p>
+    * 用户信息 服务类接口
+    * </p>
+*
+* @author YKH
+* @since 2023-06-11
+*/
+public interface UserService extends IService<User> {
+
+    Boolean add(User user);
+    void deleteByIds(String ids);
+    Boolean update(User user);
+    User getByValue(String value,String name);
+    List<User> listByValue(String value,String name);
+    Boolean updateUsefulByIds(String id, Boolean flag);
+    Page<User> page(Integer pageNum, Integer pageSize, String name);
+}
